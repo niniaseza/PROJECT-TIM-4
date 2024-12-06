b53eb134bda29b1e72d7a4aee30669d431ed5a3d
rozatunnabila <rozatun24@mhs.usk.ac.id>
2024-12-06 11:04:20 +0700

    Update projekpemro.c

--- projekpemro.c
+++ projekpemro.c
@@ -3,26 +3,29 @@
 #include <string.h>
 #include "library.h"
 
-int main(int argc, char *argv[]) {
+int main(int argc, char *argv[]) 
+{
     struct User currentUser;
     const char *alatLabFile = "alat_lab.txt";
     const char *riwayatFile = "riwayat_peminjaman.txt";
-    const char *adminFile = "data_admin.txt";
-    const char *userFile = "data_user.txt";
+    const char *adminFile   = "data_admin.txt";
+    const char *userFile    = "data_user.txt";
 
     // Cek jumlah argumen
-    if (argc < 2) {
+    if (argc < 2)
+    {
         printf("Cara Penggunaan:\n");
         printf("  Registrasi: register <role> <Nama> <password>\n");
         printf("  Login     : login <role> <Nama> <password>\n");
         return 1;
     }
-
     const char *action = argv[1];
-
+    
     // Handle registrasi
-    if (strcmp(action, "register") == 0) {
-        if (argc != 5) {
+    if (strcmp(action, "register") == 0) 
+    {
+        if (argc != 5) 
+        {
             printf("Cara Penggunaan: %s register <role> <Nama> <password>\n", argv[0]);
             return 1;
         }
@@ -35,10 +38,12 @@ int main(int argc, char *argv[]) {
         strcpy(newUser.Nama, Nama);
         strcpy(newUser.password, password);
 
-        if (strcmp(role, "admin") == 0) {
+        if (strcmp(role, "admin") == 0)
+        {
             dataUserKeFile(adminFile, &newUser);
             printf("Admin '%s' berhasil didaftarkan.\n", Nama);
-        } else if (strcmp(role, "user") == 0) {
+        } else if (strcmp(role, "user") == 0) 
+        {
             dataUserKeFile(userFile, &newUser);
             printf("User '%s' berhasil didaftarkan.\n", Nama);
         } else {
@@ -47,7 +52,8 @@ int main(int argc, char *argv[]) {
 
     // Handle login
     } else if (strcmp(action, "login") == 0) {
-        if (argc != 5) {
+        if (argc != 5) 
+        {
             printf("Cara Penggunaan: %s login <role> <Nama> <password>\n", argv[0]);
             return 1;
         }
@@ -62,27 +68,31 @@ int main(int argc, char *argv[]) {
 
         if (strcmp(role, "admin") == 0) {
             loginSuccess = loginUser(adminFile, &currentUser);
-            if (loginSuccess) {
+            if (loginSuccess) 
+            {
                 printf("Admin '%s' berhasil masuk.\n", Nama);
                 menuAdmin(alatLabFile);
-            } else {
+            } else 
+            {
                 printf("Gagal masuk sebagai Admin.\n");
             }
         } else if (strcmp(role, "user") == 0) {
             loginSuccess = loginUser(userFile, &currentUser);
-            if (loginSuccess) {
+            if (loginSuccess) 
+            {
                 printf("User '%s' berhasil masuk.\n", Nama);
                 menuUser(alatLabFile, riwayatFile, &currentUser);
             } else {
                 printf("Gagal masuk sebagai User.\n");
             }
-        } else {
+        } else 
+        {
             printf("Role tidak valid. Gunakan 'admin' atau 'user'.\n");
         }
-    } else {
+    } else 
+    {
         printf("Perintah tidak valid. Gunakan 'register' atau 'login'.\n");
     }
-
     return 0;
 }
 
