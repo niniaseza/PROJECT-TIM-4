57c3307030700bb5f85dfccc262d74e3a740de8b
rozatunnabila <rozatun24@mhs.usk.ac.id>
2024-12-06 12:06:22 +0700

    Update library.h

--- library.h
+++ library.h
@@ -7,8 +7,7 @@
 #include <time.h>
 
 //Struktur data untuk name
-struct User
-{
+struct User {
     char Nama[50];
     char password[50];
 };
@@ -17,4 +16,3 @@ struct User
 struct AlatLab {
     unsigned int idAlat;
     char namaAlat[50];
-    char merek[50];
