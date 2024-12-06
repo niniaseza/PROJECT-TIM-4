529993ad406ae4900463da6d2c85f14041337e4b
rozatunnabila <rozatun24@mhs.usk.ac.id>
2024-12-06 10:43:42 +0700

    Update projekpemro.c

--- projekpemro.c
+++ projekpemro.c
@@ -1,1 +1,44 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include "library.h"
 
+int main(int argc, char *argv[]) {
+    struct User currentUser;
+    const char *alatLabFile = "alat_lab.txt";
+    const char *riwayatFile = "riwayat_peminjaman.txt";
+    const char *adminFile = "data_admin.txt";
+    const char *userFile = "data_user.txt";
+
+    // Cek jumlah argumen
+    if (argc < 2) {
+        printf("Cara Penggunaan:\n");
+        printf("  Registrasi: register <role> <Nama> <password>\n");
+        printf("  Login     : login <role> <Nama> <password>\n");
+        return 1;
+    }
+
+    const char *action = argv[1];
+
+    // Handle registrasi
+    if (strcmp(action, "register") == 0) {
+        if (argc != 5) {
+            printf("Cara Penggunaan: %s register <role> <Nama> <password>\n", argv[0]);
+            return 1;
+        }
+
+        const char *role = argv[2];
+        const char *Nama = argv[3];
+        const char *password = argv[4];
+        struct User newUser;
+
+        strcpy(newUser.Nama, Nama);
+        strcpy(newUser.password, password);
+
+        if (strcmp(role, "admin") == 0) {
+            dataUserKeFile(adminFile, &newUser);
+            printf("Admin '%s' berhasil didaftarkan.\n", Nama);
+        } else if (strcmp(role, "user") == 0) {
+            dataUserKeFile(userFile, &newUser);
+            printf("User '%s' berhasil didaftarkan.\n", Nama);
+        } else {
